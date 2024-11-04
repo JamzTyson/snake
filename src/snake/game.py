3552398d1929cac451da54866a9549adcb697ed1
JamzTyson <jamztyson@gmail.com>
2024-11-04 02:20:16 +0000

    Tidy and simplify.
    Removed some duplicate code.
    Make a few variable names more descriptive.
    Remove redundant configuration attributes,
    move_delta_map as Snake class attribute.
    Pass configuration objects to Snake class rather than injecting.

--- src/snake/game.py
+++ src/snake/game.py
@@ -7,8 +7,10 @@ from enum import Enum, auto
 from collections import namedtuple
 
 
-Sprite = namedtuple('Sprite', ['color', 'shape'])
-Text = namedtuple('Text', ['color', 'bg_color', 'font', 'v_pos'])
+SpriteAttributes = namedtuple('SpriteAttributes',
+                              ['color', 'shape'])
+TextAttributes = namedtuple('TextAttributes',
+                            ['color', 'bg_color', 'font', 'v_pos'])
 
 
 class Direction(Enum):
@@ -20,46 +22,30 @@ class Direction(Enum):
     STOP = auto()
 
 
+class KeyBindings:
+    """Static key bindings for direction controls."""
+    bindings = {
+        Direction.UP: "Up",
+        Direction.DOWN: "Down",
+        Direction.LEFT: "Left",
+        Direction.RIGHT: "Right",
+        Direction.STOP: "space"
+    }
+
+
 @dataclass
 class Config:
     """Configure game defaults."""
-    # Game initial default settings.
-    initial_update_delay = 25  # milliseconds
-    initial_score = 0
-    initial_high_score = 0
-    move_delta = 5  # Distance to move in x,y directions per step.
+    # Game speed.
+    initial_update_delay: int = 25  # milliseconds
+    move_delta: float = 5.0  # Distance to move snake head per step.
 
     # Scoreboard.
     scoreboard_height: int = 50
-    text: Text = Text(color='white',
-                      bg_color='black',
-                      font=("sans", 20, "normal"),
-                      v_pos=50)
-
-    # Sprites.
-    head: Sprite = Sprite(color='white', shape='square')
-    segment: Sprite = Sprite(color='orange', shape='circle')
-    head_size: int = 20  # px size of head.
-    # noinspection SpellCheckingInspection
-    food: tuple[Sprite, ...] = (Sprite(color='red', shape='circle'),
-                                Sprite(color='yellow', shape='circle'),
-                                Sprite(color='limegreen', shape='circle'))
-
-    # Map direction of movement to x,y delta.
-    move_delta_map = {
-        Direction.UP: (0, move_delta),
-        Direction.DOWN: (0, -move_delta),
-        Direction.LEFT: (-move_delta, 0),
-        Direction.RIGHT: (move_delta, 0)
-    }
-
-    key_bindings = {
-        "Up": Direction.UP,
-        "Down": Direction.DOWN,
-        "Left": Direction.LEFT,
-        "Right": Direction.RIGHT,
-        "space": Direction.STOP
-    }
+    text: TextAttributes = TextAttributes(color='white',
+                                          bg_color='black',
+                                          font=("sans", 20, "normal"),
+                                          v_pos=50)
 
     # App window properties.
     display_width: int = 600
@@ -75,20 +61,43 @@ class Config:
         return self.background_color
 
 
+@dataclass
+class SpriteConfig:
+    """Game Turtle attributes."""
+    head: SpriteAttributes = SpriteAttributes(color='white',
+                                              shape='square')
+    segment: SpriteAttributes = SpriteAttributes(color='orange',
+                                                 shape='circle')
+    head_size: int = 20  # px size of head.
+    # noinspection SpellCheckingInspection
+    food: tuple[SpriteAttributes, ...] = (
+        SpriteAttributes(color='red', shape='circle'),
+        SpriteAttributes(color='yellow', shape='circle'),
+        SpriteAttributes(color='limegreen', shape='circle')
+    )
+
+
 class SnakeGame:
     """Snake game."""
 
-    def __init__(self, config):
+    def __init__(self, config: Config, sprite_config: SpriteConfig):
+        """Initialise SnakeGame.
+
+        Args:
+            config: Default configuration settings.
+            sprite_config: Sprite attributes.
+        """
         turtle.tracer(0)
         self.config = config
+        self.sprite_config = sprite_config
         # Default settings.
         width = config.display_width
         height = config.display_height
 
         # Initialise speed (delay) and scores.
         self.delay = config.initial_update_delay
-        self.score = config.initial_score
-        self.high_score = config.initial_high_score
+        self.score = 0
+        self.high_score = 0
 
         # Initialise screen.
         self.screen = turtle.Screen()
@@ -106,7 +115,7 @@ class SnakeGame:
         self.pen.hideturtle()
         self.pen.goto(0, height // 2 - config.text.v_pos)
 
-        self.snake = Snake()
+        self.snake = Snake(self.config, self.sprite_config)
         self.setup_listeners()
 
         # Add food
@@ -120,35 +129,28 @@ class SnakeGame:
         height = self.config.scoreboard_height
         width = self.config.display_width
         top = self.config.display_height // 2
-
-        scoreboard_turtle = turtle.Turtle()
-        scoreboard_turtle.hideturtle()
-        scoreboard_turtle.penup()
-        # Start at top left corner.
-        scoreboard_turtle.goto(-width // 2, top)
-        scoreboard_turtle.pendown()
-        scoreboard_turtle.color(self.config.text.bg_color)
-
-        scoreboard_turtle.begin_fill()
-        for distance in (width, height, width, height):
-            scoreboard_turtle.forward(distance)
-            scoreboard_turtle.right(90)
-        scoreboard_turtle.end_fill()
+        color = self.config.text.bg_color
+        self._draw_board_region(width, height, top, color)
 
     def draw_game_area(self):
         """Draw the area that we play in."""
         scoreboard_height = self.config.scoreboard_height
         width = self.config.display_width
-        top = self.config.display_height // 2 - scoreboard_height
         height = self.config.display_height - scoreboard_height
+        top = self.config.display_height // 2 - scoreboard_height
+        color = self.config.board_color
+        self._draw_board_region(width, height, top, color)
 
+    @staticmethod
+    def _draw_board_region(width, height, top, color):
+        """Draw rectangular area the width of the board."""
         board_turtle = turtle.Turtle()
         board_turtle.hideturtle()
         board_turtle.penup()
         # Start at top left corner.
         board_turtle.goto(-width // 2, top)
         board_turtle.pendown()
-        board_turtle.color(self.config.board_color)
+        board_turtle.color(color)
 
         board_turtle.begin_fill()
         for distance in (width, height, width, height):
@@ -159,9 +161,9 @@ class SnakeGame:
     def setup_listeners(self):
         """Configure listeners."""
         self.screen.listen()
-        for key, direction in self.config.key_bindings.items():
+        for direction, char in KeyBindings.bindings.items():
             self.screen.onkeypress(
-                lambda d=direction: self.snake.set_direction(d), key)
+                lambda d=direction: self.snake.set_direction(d), char)
 
     def update_score(self):
         """Write current score to screen."""
@@ -175,13 +177,13 @@ class SnakeGame:
         self.snake.move()
         self.check_collision()
         turtle.update()
-        self.screen.ontimer(self.update, int(self.delay))
+        self.screen.ontimer(self.update, self.delay)
 
     def check_collision(self):
         """Return True if snake collides with edge of board or its tail."""
         x_coord = self.snake.head.xcor()
         y_coord = self.snake.head.ycor()
-        half_head_size = self.config.head_size // 2
+        half_head_size = self.sprite_config.head_size // 2
         half_width = self.config.display_width // 2
 
         x_max = half_width - half_head_size
@@ -189,15 +191,14 @@ class SnakeGame:
         y_max = half_width - self.config.scoreboard_height - half_head_size
         y_min = -half_width + half_head_size
 
-        out_of_bounds = not (x_min <= x_coord <= x_max and y_min <= y_coord <= y_max)
+        out_of_bounds = not (x_min <= x_coord <= x_max and
+                             y_min <= y_coord <= y_max)
         if out_of_bounds:
             print(x_coord, y_coord)  # crash
 
 
 class Snake:
     """Snake character as compound turtle."""
-    config = Config()
-
     # Attribute to check for backtracking.
     _backtrack = {
         Direction.UP: Direction.DOWN,
@@ -206,15 +207,37 @@ class Snake:
         Direction.RIGHT: Direction.LEFT
     }
 
-    def __init__(self):
+    _move_delta_map: dict[Direction, tuple[int, int]] = {}
+
+    def __init__(self, config, sprite_config):
+        self.config = config
+        self.sprite_config = sprite_config
+
+        # Map direction of movement to x,y delta.
+        if not Snake._move_delta_map:
+            Snake.set_move_delta_map(config.move_delta)
+
         # head of the snake
         self.head = turtle.Turtle()
-        self.head.shape(Snake.config.head.shape)
-        self.head.color(Snake.config.head.color)
+        self.head.shape(sprite_config.head.shape)
+        self.head.color(sprite_config.head.color)
         self.head.penup()
         self.head.goto(0, 0)
         self.head_direction = Direction.STOP
         self.segments = []
+
+    @classmethod
+    def set_move_delta_map(cls, delta: int):
+        """Initialize the class-level movement delta map.
+
+        Maps the direction of movement to x,y delta.
+        """
+        cls._move_delta_map = {
+            Direction.UP: (0, delta),
+            Direction.DOWN: (0, -delta),
+            Direction.LEFT: (-delta, 0),
+            Direction.RIGHT: (delta, 0)
+        }
 
     def set_direction(self, direction: Direction):
         """Set snake head direction.
@@ -229,14 +252,14 @@ class Snake:
         new_segment = turtle.Turtle()
         new_segment.speed(0)
         new_segment.shape("square")
-        new_segment.color(Snake.config.segment.color)
+        new_segment.color(self.sprite_config.segment.color)
         new_segment.penup()
         self.segments.append(new_segment)
 
     def move(self):
         """Update snake position."""
         if self.head_direction is not Direction.STOP:
-            delta_x, delta_y = self.config.move_delta_map[self.head_direction]
+            delta_x, delta_y = Snake._move_delta_map[self.head_direction]
             new_x = self.head.xcor() + delta_x
             new_y = self.head.ycor() + delta_y
             self.head.goto(new_x, new_y)
@@ -244,7 +267,7 @@ class Snake:
 
 class Food(turtle.Turtle):
     """Sprites to be collected."""
-    sprites = Config().food
+    sprites = SpriteConfig.food
 
     def __init__(self):
         super().__init__()
@@ -263,7 +286,5 @@ class Food(turtle.Turtle):
 
 
 if __name__ == '__main__':
-    # Init game instance.
-    game = SnakeGame(Config())
-
+    game = SnakeGame(Config(), SpriteConfig())
     turtle.Screen().mainloop()
