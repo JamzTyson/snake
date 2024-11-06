52352e0312976612d5ede78b8c2a52127c119c11
JamzTyson <jamztyson@gmail.com>
2024-11-06 20:57:48 +0000

    Refactor: ScreenManager class and collision detection.

--- src/snake/game.py
+++ src/snake/game.py
@@ -6,7 +6,6 @@ from dataclasses import dataclass
 from enum import Enum, auto
 from itertools import pairwise
 from random import choice, randint
-
 import turtle
 
 
@@ -39,6 +38,7 @@ class KeyBindings:
 @dataclass
 class Config:
     """Configure game defaults."""
+
     # Game speed.
     initial_update_delay: int = 25  # milliseconds
     move_delta: float = 5.0  # Distance to move snake head per step.
@@ -71,7 +71,7 @@ class SpriteConfig:
                                               shape='square')
     segment: SpriteAttributes = SpriteAttributes(color='white',
                                                  shape='circle')
-    head_size: int = 20  # px size of head.
+    sprite_size: int = 20  # px size of head.
     # noinspection SpellCheckingInspection
     food: tuple[SpriteAttributes, ...] = (
         SpriteAttributes(color='red', shape='circle'),
@@ -90,42 +90,100 @@ class SnakeGame:
             config: Default configuration settings.
             sprite_config: Sprite attributes.
         """
-        turtle.tracer(0)
+
         self.config = config
         self.sprite_config = sprite_config
-        # Default settings.
-        width = config.display_width
-        height = config.display_height
 
         # Initialise speed (delay) and scores.
         self.delay = config.initial_update_delay
         self.score = 0
         self.high_score = 0
 
-        # Initialise screen.
-        self.screen = turtle.Screen()
-        # Setup screen area with border space.
-        self.screen.setup(width + config.border * 2,
-                          height + config.border * 2)
-        self.screen.title("Snake")
-        self.screen.bgcolor(config.bg)
-        self.draw_scoreboard()
-        self.draw_game_area()
-
-        # Screen text
-        self.pen = turtle.Turtle()
-        self.pen.color(config.text.color)
-        self.pen.hideturtle()
-        self.pen.goto(0, height // 2 - config.text.v_pos)
-
+        self.screen_manager = ScreenManager(config, sprite_config)
         self.snake = Snake(self.config, self.sprite_config)
         self.setup_listeners()
 
         # Add food
-        # food = Food()
-        # print(food.xcor(), food.ycor())
-        self.update_score()
+        self.food = Food(config, sprite_config)
+        print(self.food.xcor(), self.food.ycor())
+        self.screen_manager.update_score(self.score, self.high_score)
         self.update()
+
+    def setup_listeners(self) -> None:
+        """Configure listeners."""
+        self.screen_manager.screen.listen()
+        for direction, char in KeyBindings.bindings.items():
+            fun: Callable = lambda d=direction: self.snake.set_direction(d)
+            self.screen_manager.screen.onkeypress(fun, char)
+
+    def check_collision(self) -> bool:
+        """Return True if snake collides with edge of board or its tail."""
+        return self.edge_collision() or self.tail_collision()
+
+    def tail_collision(self) -> bool:
+        """Return True if head collides with edge of board."""
+        head = self.snake.head
+        half_head_size = self.sprite_config.sprite_size // 2
+        for segment in self.snake.segments:
+            if segment.distance(head) <= half_head_size:
+                return True
+        return False
+
+    def edge_collision(self) -> bool:
+        """Return True if head collides with edge of board."""
+        x_coord = self.snake.head.xcor()
+        y_coord = self.snake.head.ycor()
+        half_head_size = self.sprite_config.sprite_size // 2
+        half_width = self.config.display_width // 2
+        half_height = self.config.display_height // 2
+
+        x_min = -(half_width - half_head_size)  # Left
+        x_max = -x_min
+        y_min = -(half_height - half_head_size)  # Bottom
+        y_max = -y_min - self.config.scoreboard_height
+
+        return not (x_min <= x_coord <= x_max and
+                    y_min <= y_coord <= y_max)
+
+    def update(self) -> None:
+        """Main game loop to keep updating the game state."""
+        self.snake.move()
+        if self.check_collision():
+            self.snake.reset_snake()
+        turtle.update()  # pylint: disable=no-member
+        self.screen_manager.screen.ontimer(self.update, self.delay)
+
+
+class ScreenManager:
+    """Draw and manage screen areas."""
+    def __init__(self, config: Config, sprite_config: SpriteConfig):
+        self.config = config
+        self.sprite_config = sprite_config
+        self.screen = turtle.Screen()
+        self.pen = turtle.Turtle()
+
+        self.setup_screen()
+
+    def setup_screen(self):
+        """Setup screen properties and initialise the board."""
+        turtle.tracer(0)  # pylint: disable=no-member
+        width = self.config.display_width
+        height = self.config.display_height
+
+        self.screen = turtle.Screen()
+        # Setup screen area with border space.
+        self.screen.setup(width + self.config.border * 2,
+                          height + self.config.border * 2)
+        self.screen.title("Snake")
+        self.screen.bgcolor(self.config.bg)
+
+        self.draw_scoreboard()
+        self.draw_game_area()
+
+        # Screen text
+        self.pen.color(self.config.text.color)
+        self.pen.hideturtle()
+        self.pen.goto(0, height // 2 - self.config.text.v_pos)
 
     def draw_scoreboard(self) -> None:
         """Draw the score area at the top of the screen."""
@@ -145,7 +203,10 @@ class SnakeGame:
         self._draw_board_region(width, height, top, color)
 
     @staticmethod
-    def _draw_board_region(width: int, height: int, top: int, color: str) -> None:
+    def _draw_board_region(width: int,
+                           height: int,
+                           top: int,
+                           color: str) -> None:
         """Draw rectangular area the width of the board."""
         board_turtle = turtle.Turtle()
         board_turtle.hideturtle()
@@ -161,43 +222,12 @@ class SnakeGame:
             board_turtle.right(90)
         board_turtle.end_fill()
 
-    def setup_listeners(self) -> None:
-        """Configure listeners."""
-        self.screen.listen()
-        for direction, char in KeyBindings.bindings.items():
-            fun: Callable = lambda d=direction: self.snake.set_direction(d)
-            self.screen.onkeypress(fun, char)
-
-    def update_score(self) -> None:
+    def update_score(self, score: int, high_score: int) -> None:
         """Write current score to screen."""
         self.pen.clear()
-        self.pen.write(f"Score : {self.score}  "
-                       f"High Score : {self.high_score}", align="center",
+        self.pen.write(f"Score : {score}  "
+                       f"High Score : {high_score}", align="center",
                        font=self.config.text.font)
-
-    def update(self) -> None:
-        """Main game loop to keep updating the game state."""
-        self.snake.move()
-        self.check_collision()
-        turtle.update()
-        self.screen.ontimer(self.update, self.delay)
-
-    def check_collision(self) -> None:
-        """Return True if snake collides with edge of board or its tail."""
-        x_coord = self.snake.head.xcor()
-        y_coord = self.snake.head.ycor()
-        half_head_size = self.sprite_config.head_size // 2
-        half_width = self.config.display_width // 2
-
-        x_max = half_width - half_head_size
-        x_min = -half_width + half_head_size
-        y_max = half_width - self.config.scoreboard_height - half_head_size
-        y_min = -half_width + half_head_size
-
-        out_of_bounds = not (x_min <= x_coord <= x_max and
-                             y_min <= y_coord <= y_max)
-        if out_of_bounds:
-            print(x_coord, y_coord)  # crash
 
 
 class Snake:
@@ -220,14 +250,19 @@ class Snake:
         if not Snake._move_delta_map:
             Snake.set_move_delta_map(config.move_delta)
 
-        # head of the snake
         self.head = turtle.Turtle()
-        self.head.shape(sprite_config.head.shape)
-        self.head.color(sprite_config.head.color)
+        self.head.shape(self.sprite_config.head.shape)
+        self.head.color(self.sprite_config.head.color)
         self.head.penup()
-        self.head.goto(0, 0)
         self.head_direction = Direction.STOP
         self.segments: list[turtle.Turtle] = []
+        self.reset_snake()
+
+    def reset_snake(self):
+        """Reset snake and tail to initial state."""
+        self.head.goto(0, 0)
+        self.head_direction = Direction.STOP
+        self.segments = []
 
     @classmethod
     def set_move_delta_map(cls, delta: float) -> None:
@@ -285,7 +320,6 @@ class Snake:
 
             self.update_tail(previous_head_position)
 
-
     def update_tail(self, prev_head_position: tuple[float, float]) -> None:
         """Update positions of tail segments."""
         prev_head_x, prev_head_y = prev_head_position
@@ -300,21 +334,22 @@ class Snake:
 
 class Food(turtle.Turtle):
     """Sprites to be collected."""
-    sprites = SpriteConfig.food
-
-    def __init__(self) -> None:
+    def __init__(self, config: Config, sprite_config: SpriteConfig) -> None:
         super().__init__()
-        self.color(choice(Food.sprites).color)
-        self.shape('circle')
+        self.config = config
+        self.sprite_size = sprite_config.sprite_size
+        sprite = choice(sprite_config.food)
+        self.color(sprite.color)
+        self.shape(sprite.shape)
         self.penup()
+        self.place_food()
 
-        # Set position
-        padding = 20
-        x_max = Config.display_width // 2 - padding
-        x_min = - x_max
-        y_max = Config.display_height // 2 - padding
-        y_min = - y_max
-        position = randint(x_min, x_max), randint(y_min, y_max)
+    def place_food(self) -> None:
+        """Add food item at random position"""
+        padding = self.sprite_size
+        x_max = self.config.display_width // 2 - padding
+        y_max = self.config.display_height // 2 - padding
+        position = randint(-x_max, x_max), randint(-y_max, y_max)
         self.goto(position)
 
 
